898534f39d0264d2feca4b71156725bf0e130154
subhasish20 <subhasishjena8280@gmail.com>
2026-02-05 10:48:08 +0530

    solution

---
+++ 3379 Transformed Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                res[i] = nums[i];
+            } else {
+                res[i] = nums[((i + nums[i]) % n + n) % n];
+            }
+        }
+        return res;
+    }
+};
