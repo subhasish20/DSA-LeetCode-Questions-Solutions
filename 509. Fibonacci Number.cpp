9a0b90286c22012b1e541c0930e2b0338a03c39d
Subhasish Jena <Subhasishjena8280@gmail.com>
2026-07-09 12:20:25 +0530

    LeetCode509

--- 509. Fibonacci Number.cpp
+++ 509. Fibonacci Number.cpp
@@ -17,3 +17,21 @@ public:
         return prev1;
     }
 };
+
+
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if (n <= 1) return n;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
+    }
+};
