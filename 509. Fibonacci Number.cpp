54f0082dfe1c24a4374954b01c01709e14fec7cc
Subhasish Jena <Subhasishjena8280@gmail.com>
2026-07-09 12:20:04 +0530

    LeetCode509

---
+++ 509. Fibonacci Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) return n;
+
+        int prev2 = 0, prev1 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
